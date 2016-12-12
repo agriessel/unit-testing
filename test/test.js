1aa828b35498644967a218d29655d2660375c68f
Alex Griessel <alex.griessel@gmail.com>
2016-12-12 11:54:35 +0200

    Added failed test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var chai = require("chai");
+var assert = chai.assert;
+
+describe("some test here", function() {
+  it("Just testing", function() {
+    assert.equal(1, 1+1,"1+1 should = 2");
+  });
+});
